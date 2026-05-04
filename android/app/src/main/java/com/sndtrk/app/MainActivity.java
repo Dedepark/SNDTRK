d9e953806f1be851fa2d486eff46eb3172bb022c
Dede <zadpropc@gmail.com>
2026-05-05 01:17:07 +0700

    perbaikan gradle

--- android/app/src/main/java/com/sndtrk/app/MainActivity.java
+++ android/app/src/main/java/com/sndtrk/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sndtrk.app;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
@@ -8,15 +9,33 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Kunci WebView agar tetap bisa muter audio meski di background
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            // Izinkan media muter tanpa interaksi user setelah balik dari background
+            settings.setMediaPlaybackRequiresUserGesture(false);
+            // Kunci layar tetap ON lewat level sistem WebView
+            webView.setKeepScreenOn(true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // Jangan panggil super.onPause() untuk WebView agar musik nggak keputus
+        // Tapi kita tetap panggil BridgeActivity-nya biar nggak crash
+        super.onPause();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        // Memaksa WebView agar tidak mati saat aplikasi di latar belakang
+        // Paksa resume audio context kalau sempat tersendat
         WebView webView = this.bridge.getWebView();
         if (webView != null) {
-            webView.setKeepScreenOn(true);
+            webView.onResume();
+            webView.resumeTimers();
         }
     }
 }
