997803b73c81a2004e220d1d81c5493646fabc88
Dede <zadpropc@gmail.com>
2026-05-04 23:04:13 +0700

    Tambah izin background music Android

---
+++ android/app/src/main/java/com/sndtrk/app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sndtrk.app; // Pastikan ini sama dengan folder kamu
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Memaksa WebView agar tidak mati saat aplikasi di latar belakang
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            webView.setKeepScreenOn(true);
+        }
+    }
+}
